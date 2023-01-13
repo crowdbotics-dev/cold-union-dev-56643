422543a262014da1d873c3a024e3f26be8728c10
Crowdbotics <engineering@crowdbotics.com>
2023-01-13 06:07:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cold_union_dev_56643/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cold_union_dev_56643;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cold_union_dev_56643";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cold-union-dev-56643",
+  url: "https://cold-union-dev-56643.botics.co",
+  api: "https://cold-union-dev-56643.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
